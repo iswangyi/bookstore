b8724442c98824671646b7eccc8caffe30eb851a
name <email>
2021-12-02 18:59:24 +0800

    first commit

---
+++ internal/handler/createuserhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"bookstore/internal/logic"
+	"bookstore/internal/svc"
+	"bookstore/internal/types"
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+func CreateUserHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewCreateUserLogic(r.Context(), ctx)
+		err := l.CreateUser(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.Ok(w)
+		}
+	}
+}
---
+++ internal/handler/getuserhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"bookstore/internal/logic"
+	"bookstore/internal/svc"
+	"bookstore/internal/types"
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+func GetUserHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewGetUserLogic(r.Context(), ctx)
+		resp, err := l.GetUser(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
